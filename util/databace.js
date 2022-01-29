27711462369d17297b2c46d10c232fb338a540d8
GildartsClive <cmicmuir@gmail.com>
2022-01-29 00:22:58 -0700

    fisrt part for week 4

---
+++ util/databace.js
@@ -0,0 +1,26 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let _db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Shoptest:mvIUXHKMEXVwbzOW@cluster0.07x07.mongodb.net/shop?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+    throw 'No Database Found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
